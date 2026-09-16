8e13132c662b71bb7c2c230c576e8508bd18187e
Belousov Vadim <vadim.belousov.2000@inbox.ru>
2026-09-16 12:11:09 +0300

    [POLICY][LRU] Implement LRU policy logic and basic API

--- policies/LRU.hpp
+++ policies/LRU.hpp
@@ -1,38 +1,63 @@
 #pragma once
 
+#include <algorithm>
 #include <iostream>
+#include <list>
 
 #include "../include/CachePolicy.hpp"
 
 template <typename KeyType> class LRUPolicy : CachePolicy<KeyType> {
 
 public:
-  LRUPolicy() {};
+  LRUPolicy(std::size_t Size) : Capacity_(Size) {};
 
-  std::string getPolicyName() override {
-    std::cerr << "Not impemented\n";
-    return "\n";
-  }
+  std::string getPolicyName() override { return "LRU"; }
 
-  bool needInsertInCache(const KeyType &Key) override {
-    std::cerr << "Not impemented\n";
-    return true;
-  }
+  bool needInsertInCache(const KeyType &Key) override { return true; }
 
   void onCacheInsert(const KeyType &Key) override {
-    std::cerr << "Not impemented\n";
+    if (List_.size() < Capacity_) {
+      List_.push_front(Key);
+    } else {
+      std::cerr << "[LRU] Error: try to insert extra element (corrupted)\n";
+    }
   }
 
   void onCacheHit(const KeyType &Key) override {
-    std::cerr << "Not impemented\n";
+    if (List_.size() < Capacity_) {
+      auto It = std::find(List_.begin(), List_.end(), Key);
+      if (It != List_.end()) {
+        List_.splice(List_.begin(), List_, It);
+      } else {
+        std::cerr
+            << "[LRU] Error: try to splice undefined element (corrupted)\n";
+      }
+    } else {
+      std::cerr << "[LRU] Error: try to splice extra element (corrupted)\n";
+    }
   }
 
   void onCacheErase(const KeyType &Key) override {
-    std::cerr << "Not impemented\n";
+    auto It = std::find(List_.begin(), List_.end(), Key);
+		
+    if (It != List_.end()) {
+      List_.erase(It);
+    } else {
+      std::cerr << "[LRU] Error: try to delete undefined element \n";
+    }
   }
 
   std::optional<KeyType> selectVictim(void) override {
-    std::cerr << "Not impemented\n";
-    return std::nullopt;
+    if (!List_.empty()) {
+			return --(List_.end());
+		} else {
+			std::cerr << "[LRU] Warning: try to search victim in empty cache\n";
+    	return std::nullopt;
+		}
   }
+
+private:
+  std::list<KeyType> List_;
+
+  std::size_t Capacity_;
 };
